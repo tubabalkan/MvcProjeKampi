8170faf557c71b3d0a44b59b15289362d629b463
tuba balkan <tubabalkan165@gmail.com>
2024-07-07 14:35:13 +0300

    ........

---
+++ MvcProjeKampi/Controllers/ChartController.cs
@@ -0,0 +1,50 @@
+﻿using EntityLayer.Concrete;
+using MvcProjeKampi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<CategoryClass> BlogList()
+        {
+            List<CategoryClass> ct = new List<CategoryClass>();
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Yazılım",
+                CategoryCount = 10,
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Seyehat",
+                CategoryCount = 4,
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Teknoloji",
+                CategoryCount = 15,
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Spor",
+                CategoryCount = 1,
+            });
+            return ct;
+        }
+    }
+}
